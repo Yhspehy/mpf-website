905bb0151661ba28b52c0f17fa6e604685d95c88
Yhspehy <yhspehy@gmail.com>
2024-09-03 20:42:31 +0800

    feat: update mpf info

--- src/views/mpf/Info.vue
+++ src/views/mpf/Info.vue
@@ -51,7 +51,7 @@ const p3 = `联系人：
         "框架下港航合作开放交 流和互学互鉴平台，推动建设全球港航命运共同体。
       </div>
       <div class="text">"平等、合作、互利、共赢 "的宗旨，遵循 "共商、共建、共享"的原则。</div>
-      <img src="/public/new_img/1.png" alt="" class="w-full" />
+      <img src="https://pic.imgdb.cn/item/66d70232d9c307b7e9a172c2.png" alt="" class="w-full" />
       <div class="subTitle">
         <div class="title-circle"></div>
         平等
@@ -82,8 +82,12 @@ const p3 = `联系人：
         共赢
       </div>
       <div class="text">论坛促进与会代表达成共赢的合作共识。</div>
-      <img src="/public/new_img/2.png" alt="" class="w-full" />
-      <img src="/public/new_img/图1.jpg" alt="" class="w-full rd-8px" />
+      <img src="https://pic.imgdb.cn/item/66d70233d9c307b7e9a17400.jpg" alt="" class="w-full" />
+      <img
+        src="https://pic.imgdb.cn/item/66d70208d9c307b7e9a15045.jpg"
+        alt=""
+        class="w-full rd-8px"
+      />
     </div>
 
     <div class="card">
@@ -99,7 +103,7 @@ const p3 = `联系人：
         展趋势，凝聚开放合作共识，谋求服务构建新发展格局 和推动世界经贸发展。
       </div>
 
-      <img src="/public/new_img/3.png" alt="" class="w-full" />
+      <img src="https://pic.imgdb.cn/item/66d70234d9c307b7e9a1751d.jpg" alt="" class="w-full" />
 
       <div class="text">
         涵盖50%以上的共建“一带一路”国家和地区、60%的全球前10大码头运营商、65%的全球前20大货代公司、90%的全球前20大班轮公司、95%的全球前40大集装箱港口、100%的全球前10大国际性航运中心、100%的国内省级港口集团。
@@ -128,10 +132,18 @@ const p3 = `联系人：
           >论坛上形 成的核心成果被纳入国家第二、三届 "一带一路 " 国际合作高峰论坛成果清单。</span
         >
       </div>
-      <img src="/public/new_img/4.png" alt="" class="w-full" />
+      <img src="https://pic.imgdb.cn/item/66d70232d9c307b7e9a1722f.jpg" alt="" class="w-full" />
 
-      <img src="/public/new_img/5.png" alt="" class="w-full mt-10px" />
-      <img src="/public/new_img/图2.jpg" alt="" class="w-full rd-8px" />
+      <img
+        src="https://pic.imgdb.cn/item/66d70232d9c307b7e9a17270.jpg"
+        alt=""
+        class="w-full mt-10px"
+      />
+      <img
+        src="https://pic.imgdb.cn/item/66d70209d9c307b7e9a151a1.jpg"
+        alt=""
+        class="w-full rd-8px"
+      />
     </div>
 
     <div class="card">
@@ -144,15 +156,28 @@ const p3 = `联系人：
         海丝论坛将舞台剧、现场书画篆刻表演、创新展 等艺术形式搬上论坛舞台，并提供港区参观，小
         型业务洽谈等场外活动安排。
       </div>
-      <img src="/public/new_img/6.png" alt="" class="w-full mt-10px" />
+      <img
+        src="https://pic.imgdb.cn/item/66d7023ad9c307b7e9a17ab6.jpg"
+        alt=""
+        class="w-full mt-10px"
+      />
       <div class="flex mb-10px">
-        <img src="/public/new_img/7.png" alt="" class="h-65px object-contain" style="flex: 2" />
         <img
-          src="/public/new_img/图3.jpg"
+          src="https://pic.imgdb.cn/item/66d7023ad9c307b7e9a17ad8.jpg"
+          alt=""
+          class="h-65px object-contain"
+          style="flex: 2"
+        />
+        <img
+          src="https://pic.imgdb.cn/item/66d70207d9c307b7e9a14ed7.jpg"
           alt=""
           class="w-83px h-65px object-cover rd-10px mx-4px"
         />
-        <img src="/public/new_img/图4.jpg" alt="" class="w-83px h-65px object-cover rd-10px" />
+        <img
+          src="https://pic.imgdb.cn/item/66d70208d9c307b7e9a14f8c.jpg"
+          alt=""
+          class="w-83px h-65px object-cover rd-10px"
+        />
       </div>
 
       <div class="subTitle">02.温暖的力量</div>
@@ -160,10 +185,22 @@ const p3 = `联系人：
         海丝论坛为所有与会嘉宾提供贴心的服务，专门 组建了一支经过专业培训的志愿者团队，提供专
         业的解说，嘉宾注册，现场支持等保障服务。
       </div>
-      <img src="/public/new_img/图5.jpg" alt="" class="w-full rd-10px" />
+      <img
+        src="https://pic.imgdb.cn/item/66d70208d9c307b7e9a14ffb.jpg"
+        alt=""
+        class="w-full rd-10px"
+      />
       <div class="flex-center mb-20px">
-        <img src="/public/new_img/9.png" alt="" class="w-166px object-contain rd-10px" />
-        <img src="/public/new_img/8.png" alt="" class="w-166px object-contain rd-10px mr-4px" />
+        <img
+          src="https://pic.imgdb.cn/item/66d7023ad9c307b7e9a17a94.jpg"
+          alt=""
+          class="w-166px object-contain rd-10px"
+        />
+        <img
+          src="https://pic.imgdb.cn/item/66d7023ad9c307b7e9a17b04.jpg"
+          alt=""
+          class="w-166px object-contain rd-10px mr-4px"
+        />
       </div>
 
       <div class="subTitle">03.共同的关注</div>
@@ -175,20 +212,44 @@ const p3 = `联系人：
       <div class="text mb-20px!">
         海丝论坛通过商业合作吸纳各行业赞助合作商，为合作伙伴提供招商洽谈展位、互动视频展示、联动活动赞助等，机械工程、物流货代、能源科技、金融保险等多个与港航产业关联的企业主体广泛参与进来，展示形象、推介产品、扩大销售、实现交易。
       </div>
-      <img src="/public/new_img/图9.JPG" alt="" class="w-full rd-10px" />
+      <img
+        src="https://pic.imgdb.cn/item/66d70215d9c307b7e9a15af1.jpg"
+        alt=""
+        class="w-full rd-10px"
+      />
       <div class="flex-center mb-20px">
-        <img src="/public/new_img/图10.jpg" alt="" class="w-166px object-contain rd-10px" />
-        <img src="/public/new_img/图11.JPG" alt="" class="w-166px object-contain rd-10px mr-4px" />
+        <img
+          src="https://pic.imgdb.cn/item/66d70214d9c307b7e9a159d1.jpg"
+          alt=""
+          class="w-166px object-contain rd-10px"
+        />
+        <img
+          src="https://pic.imgdb.cn/item/66d7021ad9c307b7e9a1607b.jpg"
+          alt=""
+          class="w-166px object-contain rd-10px mr-4px"
+        />
       </div>
 
       <div class="subTitle">05.丰富的文创</div>
       <div class="text mb-20px!">
         海丝论坛推出系列文创，30余款论坛衍生周边可供选择。以论坛主视觉为设计理念，作为论坛留念，让海丝记忆传播和珍藏。
       </div>
-      <img src="/public/new_img/图6.JPG" alt="" class="w-full rd-10px" />
+      <img
+        src="https://pic.imgdb.cn/item/66d70214d9c307b7e9a159fd.jpg"
+        alt=""
+        class="w-full rd-10px"
+      />
       <div class="flex-center">
-        <img src="/public/new_img/图7.JPG" alt="" class="w-166px object-contain rd-10px" />
-        <img src="/public/new_img/图8.JPG" alt="" class="w-166px object-contain rd-10px mr-4px" />
+        <img
+          src="https://pic.imgdb.cn/item/66d70215d9c307b7e9a15a54.jpg"
+          alt=""
+          class="w-166px object-contain rd-10px"
+        />
+        <img
+          src="https://pic.imgdb.cn/item/66d70215d9c307b7e9a15aa7.jpg"
+          alt=""
+          class="w-166px object-contain rd-10px mr-4px"
+        />
       </div>
     </div>
   </div>
